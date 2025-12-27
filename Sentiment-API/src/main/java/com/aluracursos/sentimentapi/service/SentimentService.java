88308ff271b091fa31be619dc7b1c752a91e67d2
IngRodrigoPena <ingrodrigopena@gmail.com>
2025-12-26 22:49:45 -0600

    Refactor: alinear Controller y Service con contrato DTO

--- Sentiment-API/src/main/java/com/aluracursos/sentimentapi/service/SentimentService.java
+++ Sentiment-API/src/main/java/com/aluracursos/sentimentapi/service/SentimentService.java
@@ -1,13 +1,20 @@
 package com.aluracursos.sentimentapi.service;
-
+import com.aluracursos.sentimentapi.dto.SentimentRequest;
 import com.aluracursos.sentimentapi.dto.SentimentResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SentimentService {
 
-    public SentimentResponse analyze(String text) {
-        // Mock por ahora
-        return new SentimentResponse("positivo", 0.87);
+    public SentimentResponse analyze(SentimentRequest request) {
+        // Paso 1: extraer el texto dentro del service
+        String text = request.getText();
+
+        // Paso 2: lógica mock (temporal)
+        String prediccion = "positivo";
+        double probabilidad = 0.87;
+
+        // Paso 3: construir respuesta
+        return new SentimentResponse(prediccion, probabilidad);
     }
 }
